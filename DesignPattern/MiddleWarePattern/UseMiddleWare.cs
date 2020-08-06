b3f840f8a543d82d8bc58e421ae0e417750204b9
肖洋 <953567835@qq.com>
2020-08-06 09:14:50 +0800

    -

---
+++ DesignPattern/MiddleWarePattern/UseMiddleWare.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern.MiddleWarePattern
+{
+    public class UseMiddleWare
+    {
+        public class RequestContext
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public static void MiddleWareAnalysis()
+        {
+            Console.WriteLine(" ** 模拟 Middleware **");
+            var context = new RequestContext() { name = "xx", id = 1 };
+            IList<Func<Action<RequestContext>, Action<RequestContext>>> _pipeline
+               = new List<Func<Action<RequestContext>, Action<RequestContext>>>();
+            Action<RequestContext, Action> ac1 = (context, next) =>
+            {
+                Console.WriteLine("1 Before");
+                next();
+                Console.WriteLine("1 After");
+            };
+            Action<RequestContext, Action> ac2 = (context, next) =>
+            {
+                Console.WriteLine("2 Before");
+                next();
+                Console.WriteLine("2 After");
+            };
+
+            Func<Action<RequestContext>, Action<RequestContext>> func1 = (next) =>
+            context =>
+            ac1(context, () => next(context));
+            _pipeline.Add(func1);
+
+            Func<Action<RequestContext>, Action<RequestContext>> func2 = (next) =>
+            context =>
+            ac2(context, () => next(context));
+            _pipeline.Add(func2);
+
+            Action<RequestContext> ac = context => Console.WriteLine($"{nameof(context.name)} {context.name}");
+            foreach (var item in _pipeline.Reverse())
+                ac = item(ac);
+            ac(context);
+        }
+
+        public static void BuildUse()
+        {
+            Console.WriteLine(" *** Use MiddleWare ***");
+            var context = new RequestContext() { name = "xx", id = 1 };
+            var builder = PipelineBuilder<RequestContext>.Create(context =>
+            {
+                Console.WriteLine($"{nameof(context.name)} {context.name} {nameof(context.id)} {context.id}");
+            })
+              .Use((context, next) =>
+              {
+                  Console.WriteLine("1 Before");
+                  next();
+                  Console.WriteLine("1 After");
+              })
+               .Use((context, next) =>
+               {
+                   Console.WriteLine("2 Before");
+                   next();
+                   Console.WriteLine("2 After");
+               })
+             // .Run((context) =>
+             //{
+             //    Console.WriteLine("3 Before");
+             //    // next();
+             //    Console.WriteLine("3 After");
+             //}).Use((context, next) =>
+             //{
+             //    Console.WriteLine("4 Before");
+             //    // next();
+             //    Console.WriteLine("4 After");
+             //})
+             ;
+
+            var app = builder.Build();
+
+            app(context);
+            Console.ReadLine();
+        }
+    }
+}
--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -9,83 +9,13 @@ namespace MainConsole
     {
         static void Main(string[] args)
         {
-            #region 模拟 Middleware
-            Console.WriteLine(" ** 模拟 Middleware **");
-            var context = new RequestContext() { name = "xx", id = 1 };
-            IList<Func<Action<RequestContext>, Action<RequestContext>>> _pipeline
-               = new List<Func<Action<RequestContext>, Action<RequestContext>>>();
-            Action<RequestContext, Action> ac1 = (context, next) =>
-            {
-                Console.WriteLine("1 Before");
-                next();
-                Console.WriteLine("1 After");
-            };
-            Action<RequestContext, Action> ac2 = (context, next) =>
-            {
-                Console.WriteLine("2 Before");
-                next();
-                Console.WriteLine("2 After");
-            };
-
-            Func<Action<RequestContext>, Action<RequestContext>> func1 = (next) =>
-            context =>
-            ac1(context, () => next(context));
-            _pipeline.Add(func1);
-
-            Func<Action<RequestContext>, Action<RequestContext>> func2 = (next) =>
-            context =>
-            ac2(context, () => next(context));
-            _pipeline.Add(func2);
-
-            Action<RequestContext> ac = context => Console.WriteLine($"{nameof(context.name)} {context.name}");
-            foreach (var item in _pipeline.Reverse())
-                ac = item(ac);
-            ac(context);
-            //    Console.ReadKey();
+            #region 中间件模式，解析管道模型
+            DesignPattern.MiddleWarePattern.UseMiddleWare.MiddleWareAnalysis();
+            DesignPattern.MiddleWarePattern.UseMiddleWare.BuildUse();
             #endregion
 
-            #region Use MiddleWare
-            Console.WriteLine(" *** Use MiddleWare ***");
-            var builder = PipelineBuilder<RequestContext>.Create(context =>
-            {
-                Console.WriteLine($"{nameof(context.name)} {context.name} {nameof(context.id)} {context.id}");
-            })
-              .Use((context, next) =>
-              {
-                  Console.WriteLine("1 Before");
-                  next();
-                  Console.WriteLine("1 After");
-              })
-               .Use((context, next) =>
-               {
-                   Console.WriteLine("2 Before");
-                   next();
-                   Console.WriteLine("2 After");
-               })
-             // .Run((context) =>
-             //{
-             //    Console.WriteLine("3 Before");
-             //    // next();
-             //    Console.WriteLine("3 After");
-             //}).Use((context, next) =>
-             //{
-             //    Console.WriteLine("4 Before");
-             //    // next();
-             //    Console.WriteLine("4 After");
-             //})
-             ;
-
-            var app = builder.Build();
-
-            app(context);
-            Console.ReadLine();
-            #endregion
         }
     }
 
-    public class RequestContext
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
+
 }

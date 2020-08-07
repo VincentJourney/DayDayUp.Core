6be1ab526f9ca4477fda5890c78b105df41f87e5
肖洋 <953567835@qq.com>
2020-08-07 08:56:46 +0800

    fix baseHanlder

--- DesignPattern/MiddleWarePattern/MiddleWareEvent.cs
+++ DesignPattern/MiddleWarePattern/MiddleWareEvent.cs
@@ -7,16 +7,11 @@ namespace DesignPattern.MiddleWarePattern
     public abstract class MiddleWareEvent
     {
         public virtual void ActionExcutingEventHanlder(object obj)
-        {
+        => Console.WriteLine("Excuting");
 
-        }
         public virtual void ActionExcutedEventHanlder(object obj)
-        {
-
-        }
+        => Console.WriteLine("Excuted");
         public virtual void ExceptionEventHanlder(object obj)
-        {
-
-        }
+        => Console.WriteLine("Exception");
     }
 }

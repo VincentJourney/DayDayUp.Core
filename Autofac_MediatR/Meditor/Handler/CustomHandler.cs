12c2d883d575e17fba28a70358af7094c449f57b
肖洋 <953567835@qq.com>
2020-11-06 17:03:38 +0800

    clean

--- Autofac_MediatR/Meditor/Handler/CustomHandler.cs
+++ Autofac_MediatR/Meditor/Handler/CustomHandler.cs
@@ -6,23 +6,8 @@ using MediatR.Pipeline;
 
 namespace Autofac_MediatR
 {
-
-    //public class CustomHandler2 : IRequestHandler<CustomRequest, string>
-    //{
-    //    public Task<string> Handle(CustomRequest request, CancellationToken cancellationToken)
-    //    {
-    //        System.Console.WriteLine(request.a);
-    //        return Task.FromResult("2222222222222222");
-    //    }
-    //}
     public class CustomHandler : AsyncRequestHandler<CustomRequest>
     {
-        //public Task<string> Handle(CustomRequest request, CancellationToken cancellationToken)
-        //{
-        //    Thread.Sleep(5000);
-        //    return Task.FromResult("111111111111");
-        //}
-
         protected override async Task Handle(CustomRequest request, CancellationToken cancellationToken)
         {
             throw new System.Exception("send Handle Error");
